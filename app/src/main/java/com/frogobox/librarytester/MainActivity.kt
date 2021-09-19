ada059267a2fc4e1d054161d67941ee6cefa36a1
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-19 15:11:46 +0700

    add: jitpack-library-groovy-guide lib sample

--- app/src/main/java/com/frogobox/librarytester/MainActivity.kt
+++ app/src/main/java/com/frogobox/librarytester/MainActivity.kt
@@ -2,10 +2,21 @@ package com.frogobox.librarytester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.frogobox.androidlibfirst.FirstLibrary
+import com.frogobox.androidlibsecond.SecondLibrary
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // com.frogobox.androidlibfirst
+        val first = FirstLibrary.TAG
+
+        // com.frogobox.androidlibsecond
+        val second = SecondLibrary.TAG
+
     }
+
 }
